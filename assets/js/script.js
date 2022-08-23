7d5f91f92f8d73ad64eaba884bbeebe37628631f
William Sesselman <will@Williams-MBP.fios-router.home>
2022-08-23 16:55:07 -0400

    push before tutor

---
+++ assets/js/script.js
@@ -0,0 +1,81 @@
+// Global Variables
+var homePageDiv = document.getElementById("home")
+var quizPageDiv = document.getElementById("quiz-page")
+var postGameDiv = document.getElementById("postGame")
+var startButton = document.getElementById("startBtn")
+var highScoreButton = document.getElementById("hsBtn")
+var quizTimer = document.getElementById("timer")
+
+var quizQuestions = [{
+    question: "Which character always ends a statement?",
+    choiceA: ",",
+    choiceB: ".",
+    choiceC: "}",
+    choiceD: ";",
+    correctAnswer: "d"},
+ {
+    question: "Which character always ends a statement?",
+    choiceA: ",",
+    choiceB: ".",
+    choiceC: "}",
+    choiceD: ";",
+    correctAnswer: "d"},
+{
+    question: "Which character always ends a statement?",
+    choiceA: ",",
+    choiceB: ".",
+    choiceC: "}",
+    choiceD: ";",
+    correctAnswer: "d"},
+{
+    question: "Which character always ends a statement?",
+    choiceA: ",",
+    choiceB: ".",
+    choiceC: "}",
+    choiceD: ";",
+    correctAnswer: "d"},
+{
+    question: "Which character always ends a statement?",
+    choiceA: ",",
+    choiceB: ".",
+    choiceC: "}",
+    choiceD: ";",
+    correctAnswer: "d"},
+{
+    question: "Which character always ends a statement?",
+    choiceA: ",",
+    choiceB: ".",
+    choiceC: "}",
+    choiceD: ";",
+    correctAnswer: "d"},
+{
+    question: "Which character always ends a statement?",
+    choiceA: ",",
+    choiceB: ".",
+    choiceC: "}",
+    choiceD: ";",
+    correctAnswer: "d"},
+{
+    question: "Which character always ends a statement?",
+    choiceA: ",",
+    choiceB: ".",
+    choiceC: "}",
+    choiceD: ";",
+    correctAnswer: "d"},
+{
+    question: "Which character always ends a statement?",
+    choiceA: ",",
+    choiceB: ".",
+    choiceC: "}",
+    choiceD: ";",
+    correctAnswer: "d"},
+{
+    question: "Which character always ends a statement?",
+    choiceA: ",",
+    choiceB: ".",
+    choiceC: "}",
+    choiceD: ";",
+    correctAnswer: "d"},
+ 
+}]
+
